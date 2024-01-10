df141e7c2490bb511189d93927d74f095575fac7
Mirilice <mariaalicepco10@gmail.com>
2024-01-09 22:28:39 -0300

    Ajustes no codigo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Ipho
+Smartphone nokia3310 = new Nokia("1234567890", "Nokia XYZ", "356987012345678", 64);
+Smartphone iphone12pro = new Iphone("9876543210", "iPhone 12 Pro", "490154203237518", 128);
+
+Console.WriteLine("Nokia:");
+nokia3310.Ligar();
+nokia3310.InstalarAplicativo("Telegram");
+nokia3310.ReceberLigacao();
+
+Console.WriteLine("\nIphone:");
+iphone12pro.Ligar();
+iphone12pro.InstalarAplicativo("WhatsApp");
+iphone12pro.ReceberLigacao();
